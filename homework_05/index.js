927bc078b8cd13f220c90de075393eeccc5f8140
dikky88 <hellbentt@gmail.com>
2021-10-24 23:27:00 +0500

    сделал второе задание

--- homework_05/index.js
+++ homework_05/index.js
@@ -6,3 +6,11 @@ function unique() {
 }
 
 unique()
+
+//2. На вход поступает массив, реверсировать значения (подобно методу reverse) метод reverse не использовать.
+function reverse() {
+    const arr = [1, 2, 3, 4, 5]
+    arr.forEach(function (v,i,a){a[i] = arr.length - i;})
+    console.log(arr)
+}
+reverse()
